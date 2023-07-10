fd86f7484632554b97f5372e67b54131bd6a93e0
Pavel <pavelb7c8@gmail.com>
2023-07-11 01:23:39 +0300

    Custom implementation of formset. Not complete.

--- register/forms.py
+++ register/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.widgets import HiddenInput
 from .models import Connection, Port
 
 
@@ -15,8 +16,90 @@ class EndPointEditForm(forms.Form):
     
 
 class ConnectionForm(forms.Form):
-    choises = Port.objects.all
-    connection_point = forms.ChoiceField(choices=(('a','1'), ('b','2'), ('c','3'),))
+    connection_point = forms.CharField(max_length=100, help_text='max 100 symbols', required=False)
+    # connection_point
 
 
-# class EndPoointAddFormset()
+class ManagementForm(forms.Form):
+    # Uses for save information between page reloading.
+    total_forms = forms.IntegerField(widget=HiddenInput)
+    # total_forms = forms.IntegerField()
+
+
+class CustomFormset:
+    def __init__(self, form_class, request_post=None):
+        self.form_class = form_class
+        self.request_post = request_post
+        self.total_forms = self.get_total_forms()
+        self.management_form = self.get_management_form()
+
+    def __iter__(self):
+        return iter(self.forms())
+
+    def __getitem__(self, index):
+        return self.forms()[index]
+
+    def __repr__(self):
+        return '<total_forms> :' + str(self.total_forms)
+
+    def __len__(self):
+        return len(self.forms())
+
+    def get_management_form(self):
+        if 'total_forms' in self.request_post:
+            form = ManagementForm(self.request_post)
+            form.full_clean()
+            return form
+        form = ManagementForm(data={'total_forms': '10'})
+        form.full_clean()
+        return form
+
+    def update_management_form(self):
+        form = ManagementForm(data={'total_forms': self.total_forms})
+        return form
+
+    def get_total_forms(self):
+        return self.get_management_form().cleaned_data['total_forms']
+
+    def forms(self):
+        return [(self.form_class(), i) for i in range(self.total_forms)]
+
+    def empty_form(self):
+        return self.form_class()
+
+    def add_empty_form(self):
+        self.total_forms += 1
+        self.forms()
+        self.management_form = self.update_management_form()
+
+    def delete_form(self):
+        if len(self.forms()) > 1:
+            self.total_forms -= 1
+            self.forms().pop()
+            self.management_form = self.update_management_form()
+
+
+class ConnectionFormSet(forms.BaseFormSet):
+
+    # def add_fields(self, form, index):
+    #     super(ConnectionFormSet, self).add_fields(form, index)
+    #     # form.fields['id'] = index
+    #
+    # def get_forms_and_index(self):
+    #     return zip(self.forms, range(len(self.forms)))
+    #
+    # def delete_form(self, index):
+    #     del self.forms[index]
+    #     print(self.management_form['TOTAL_FORMS'])
+    #     print(self.management_form['INITIAL_FORMS'])
+    #
+    # def add_form(self):
+    #     self.management_form['TOTAL_FORMS'] = 3
+    #     self.forms.append(self._construct_form(len(self.forms) + 1))
+    #     print(self.management_form['TOTAL_FORMS'])
+    #     print(self.management_form['INITIAL_FORMS'])
+    pass
+
+
+class EndPointAddFormset():
+    pass
--- register/views.py
+++ register/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 
 
 from .models import Equipment, EndPoint, Consumer, Communication, Connection, Port
-from .forms import forms, EndPointEditForm, ConnectionForm
+from .forms import forms, EndPointEditForm, ConnectionForm, ConnectionFormSet, CustomFormset
 # Create your views here.
 
 
@@ -103,7 +103,7 @@ def endpoint_edit(request, pk):
         help_msg = f"Enter new name for endpoint {endpoint_inst.name}"
         form = EndPointEditForm(initial={'endpoint_name': help_msg,})
 
-    return render(request, 'register/endpoint_edit.html', {'form': form, 'endpoint_inst':endpoint_inst})
+    return render(request, 'register/endpoint_edit.html', {'form': form, 'endpoint_inst': endpoint_inst})
 
 
 class CommunicationCreateView(generic.edit.CreateView):
@@ -124,15 +124,97 @@ class CommunicationDeleteView(generic.edit.DeleteView):
     success_url = reverse_lazy('communications')
 
 
-def connection_form_view(request):
-    connection_formset_factory = forms.formset_factory(ConnectionForm, extra=2)
-    
-    connection_formset = connection_formset_factory(initial=[{'connection_point': Port.objects.first()}])
+# def connection_form_view(request):
+#     # num_forms = 0
+#     prefix = 'form'
+#     if request.method == 'POST':
+#         if 'add_new_extra_field' in request.POST:
+#             print('add_new_extra_field')
+#
+#             num_forms = int(request.session.get('num_forms', 0))
+#             num_forms += 1
+#             request.session['num_forms'] = num_forms
+#
+#             print(num_forms)
+#
+#             connection_formset_factory = forms.formset_factory(ConnectionForm, extra=num_forms, can_delete=True)
+#
+#             print(request.POST)
+#             data = {}
+#             print('---------------------')
+#             for key in request.POST:
+#                 data[key] = request.POST[key]
+#                 # print(key, ': ', end=' ')
+#                 # print(request.POST[key])
+#             print('-' * 20)
+#
+#             data.update({'form-TOTAL_FORMS': str(num_forms),
+#                          'form-INITIAL_FORMS': str(num_forms - 1),
+#                          # 'form-MIN_NUM_FORMS': 1,
+#                          # 'form-MAX_NUM_FORMS': '100',
+#                          })
+#
+#             print(data)
+#             connection_formset = connection_formset_factory(data, prefix=prefix)
+#
+#             context = {
+#                 'formset': connection_formset,
+#                 'num_forms': num_forms,
+#             }
+#
+#     else:
+#         connection_formset_factory = forms.formset_factory(ConnectionForm, extra=0, can_delete=True)
+#         print(connection_formset_factory.__dict__)
+#         connection_formset = connection_formset_factory(prefix=prefix)
+#         num_forms = 0
+#         request.session['num_forms'] = num_forms
+#         context = {
+#             'formset': connection_formset,
+#             'num_forms': num_forms,
+#         }
+#         print(connection_formset)
+#     return render(request, 'connection_form.html', context=context)
 
-    # form = connection_formset
+# def connection_form_view(request):
+#     connection_formset_factory = forms.formset_factory(ConnectionForm, formset=ConnectionFormSet, extra=1, can_delete=True)
+#     print(connection_formset_factory.__dict__)
+#     connection_formset = connection_formset_factory()
+#     num_forms = 0
+#     request.session['num_forms'] = num_forms
+#     context = {
+#         'formset': connection_formset,
+#         'num_forms': num_forms,
+#         'fi': connection_formset.get_forms_and_index(),
+#     }
+#     # print('+' * 50)
+#     # print(connection_formset)
+#     if request.method == 'POST':
+#         if 'add_new_extra_field' in request.POST:
+#             connection_formset.add_form()
+#         if 'delete_first' in request.POST:
+#             connection_formset.delete_form(0)
+#     # print('=' * 50)
+#     # print(connection_formset)
+#     return render(request, 'connection_form.html', context=context)
+
+
+def connection_form_view(request):
+    my_formset = CustomFormset(ConnectionForm, request.POST)
+
+    # print('+' * 50)
+    # print(connection_formset)
+    if request.method == 'POST':
+        if 'add_new_extra_field' in request.POST:
+            print('ADD matches')
+            my_formset.add_empty_form()
+            # print(my_formset.max_index)
+        if 'delete_first' in request.POST:
+            my_formset.delete_form()
+            print('DELETE matches')
+    # print('=' * 50)
+    print(my_formset)
 
     context = {
-        'formset': connection_formset,
+        'formset': my_formset,
     }
-    
     return render(request, 'connection_form.html', context=context)
